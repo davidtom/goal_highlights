4e06634f7027433076efbc0969347d49e25c9034
davidtom <davidjtomczyk@gmail.com>
2017-07-04 15:20:04 -0400

    update method to get higlight's date; now using created_utc from redd api, not created_at from activerecord

--- app/models/highlight.rb
+++ app/models/highlight.rb
@@ -43,13 +43,18 @@ class Highlight < ActiveRecord::Base
     iframe_string = self.media_embed.scan(/<iframe.+<\/iframe>/)[0].gsub("\\","")
   end
 
+  def self.utc_to_date(utc_integer)
+    DateTime.strptime("#{utc_integer}",'%s').to_date
+  end
+
   def self.all_group_and_order_by_date
     #TODO MAKE THIS MORE DESCRIPTIVE
     #Sort goals in ascending order by 'created_utc'; group by created_at.to_date
     #Returns a hash: {created_at.to_date: [highlights]} -sorted asc
     sorted_hash = {}
     self.order("created_utc ASC").each do |highlight|
-      date = highlight.created_at.to_date
+      # date = highlight.created_at.to_date
+      date = utc_to_date(highlight.created_utc)
       sorted_hash[date] = [] if sorted_hash[date].nil?
       sorted_hash[date] << highlight
     end
